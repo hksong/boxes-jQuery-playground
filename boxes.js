0d7d97914ee26139cf1854710ba292087d2675c7
He Kun Song <hekunsong@gmail.com>
2015-08-24 17:58:56 -0700

    complete

--- boxes.js
+++ boxes.js
@@ -1,1 +1,43 @@
 console.log("hello world");
+
+// 	Exercise 2
+// $(function() {
+// 	window.alert("DOM ready");
+// 	$("#secretBox").css("background", "white");
+// 	$("#secretBox").append("<h1>secret box!</h1>");
+// 	$("#row1 > div").removeClass().addClass("box boxType3");
+// 	$("#row4 > div:last").hide();
+// 	$(".boxType1").css("background", "white");
+// 	$("#row2 div:lt(2)").removeClass();
+// 	$(".box").not("#secretBox").css("width", "2px");
+// });
+
+// Exercise 3
+$(document).ready(function() {
+	// window.addEventListener('click', function() {
+	// 	alert('only once!');
+	// 	this.removeEventListener('click', arguments.callee);
+	// });
+	// $("div").on("click", function(e) {
+	// 	console.log(e.pageX, e.pageY);
+	// });
+	// $(".boxType1").append('<a href="http://www.galvanize.com">Galvanize</a>');
+	// $("a").on("click", function(e) {
+	// 	e.preventDefault();
+	// 	alert("YOU WILL NEVER LEAVE!");
+	// });
+	// $(".box").append('<img src="http://lorempixel.com/200/200/cats/">');
+	// $("img").toggle();
+	// $(".box").on("click", function() {
+	// 	$(this).find("img").toggle("slow");
+	// });
+	// $("#container").on("click", function(e) {
+	// 	if (this === e.target) {
+	// 		$(this).css("background", "green");
+	// 	}
+	// 	else {
+	// 		$(this).css("background", "black");
+	// 		$(e.target).css("background", "white");
+	// 	}
+	// });
+});
